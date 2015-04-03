bcd31bd0dde6d7d81535b3ae1ad2a461bd930ce5
Nuttapon Pattanavijit <nuttt.p@gmail.com>
2015-04-03 11:14:58 +0700

    react test

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
   <meta charset="UTF-8">
   <title>Document</title>
+  <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <script src="bower_components/jquery/dist/jquery.min.js"></script>
+  <script src="bower_components/react/react.min.js"></script>
+  <script src="bower_components/react/JSXTransformer.js"></script>
+  
 </head>
 <body>
-  <h1>Test from git!</h1>
+  <div class="container">
+    <h1>Examples</h1>
+    <div id="finder"></div>
+
+  </div>
+  <script type="text/jsx" src="js/component.js"></script>
 </body>
 </html>
